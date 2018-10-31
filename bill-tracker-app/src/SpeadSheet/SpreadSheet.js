c08de85f3cd43524f46a345adf4e61bb8b305807
Brian Hankins <brian.a.hankins@gmail.com>
2018-10-31 09:09:43 -0500

    First commit of initial project setup

---
+++ bill-tracker-app/src/SpeadSheet/SpreadSheet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SpeadSheet extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SpeadSheet Componnent Test</h1>
+      </div>
+    );
+  }
+}
+
+export default SpeadSheet;
